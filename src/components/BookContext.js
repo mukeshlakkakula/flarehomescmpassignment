f95a36f1320ecc5e90c7f1130573443f61df69c5
Mukesh <lakkakulababblu@gmail.com>
2024-10-04 09:12:43 +0530

    changed

--- src/components/BookContext.js
+++ src/components/BookContext.js
@@ -1,12 +1,10 @@
 import React, { createContext, useState } from "react";
 
-// Create a context for the personal library
 export const BookContext = createContext();
 
 export const BookProvider = ({ children }) => {
   const [myLibrary, setMyLibrary] = useState([]);
 
-  // Add a book to the personal library
   const addBookToLibrary = (book) => {
     const filter = myLibrary.filter((each) => each.id === book.id);
 
@@ -15,7 +13,6 @@ export const BookProvider = ({ children }) => {
     }
   };
 
-  // Remove a book from the personal library
   const removeBookFromLibrary = (id) => {
     setMyLibrary(myLibrary.filter((book) => book.id !== id));
   };
